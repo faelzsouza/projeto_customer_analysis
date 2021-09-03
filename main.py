9ccc004f5ccf9366408b8634519aba998aa63e31
vinicius <viniciusmc486@gmail.com>
2021-09-03 15:11:59 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pymongo import MongoClient
 from config import mongoURI
 import pandas as pd
 
-
 client = MongoClient(mongoURI)
 db_name = client['customers_analytics']
 collection_name = db_name['customers_analytics']
